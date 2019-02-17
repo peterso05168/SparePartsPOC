0ebde768ece8bfe87a523fc4714b4a8c75cc4980
hoyhung <constanzaho@gmail.com>
2019-02-17 15:53:13 +0800

    update bna

--- SI&I/hyperledger/README.md
+++ SI&I/hyperledger/README.md
@@ -1,1 +1,16 @@
-Please add hyperledger code here.
+* Start the fabric runtime with `startFabric.sh`
+https://hyperledger.github.io/composer/latest/installing/development-tools.html
+
+* update the version property in package.json file
+
+* Repackage the business network archive (.BNA) file, from the project directory with `composer archive create -t dir -n . `
+
+* Install the network from the repackaged .BNA file with `composer network install --card PeerAdmin@hlfv1 --archiveFile NETWORK_NAME@NETWORK_VERSION.bna`
+
+* Start the business network with `composer network start --networkName NETWORK_NAME --networkVersion NETWORK_VERSION --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card`
+
+* Re-installed the admin card with `composer card import --file networkadmin.card`
+
+* Ping the network to confirm it is running and the correct version with `composer network ping --card admin@NETWORK_NAME`. Make note of the Business Network Version.
+
+* start the RESTful API `composer-rest-server -c admin@block-track -n never -u true -d n -w true`
--- SI&I/hyperledger/lib/logic.js
+++ SI&I/hyperledger/lib/logic.js
@@ -1,11 +1,48 @@
 /**
- * Trade a product to a new player
- * @param {org.hyperledger_composer.scms.MoveProduct} moveProduct - the trade product transaction
+ * Update requestor status of the contract function.
+ * @param {org.hyperledger_composer.scms.UpdateReqContract} updateReqContract
  * @transaction
  */
-async function moveProduct(moveProduct) { // eslint-disable-line no-unused-vars
-    moveProduct.product.issuer = moveProduct.product.owner;
-    moveProduct.product.owner = moveProduct.newOwner;
-    const assetRegistry = await getAssetRegistry('org.hyperledger_composer.scms.Product');
-    await assetRegistry.update(moveProduct.product);
-   }
+async function updateReqContract(updateReqContract) {  
+    //Update the value in the contract
+    let contract = updateReqContract.contract;
+    contract.requestorStatus = updateReqContract.newStatus;
+    // Get the asset registry that stores the assets. Note that
+    // getAssetRegistry() returns a promise, so we have to await for it.
+    let assetRegistry = await getAssetRegistry('org.hyperledger_composer.scms.Contract');
+    
+    await assetRegistry.update(contract);
+  }
+  
+  /**
+   * Update respondor status of the contract function.
+   * @param {org.hyperledger_composer.scms.UpdateRespContract} updateRespContract
+   * @transaction
+   */
+  async function updateRespContract(updateRespContract) {  
+    //Update the value in the contract
+    let contract = updateRespContract.contract;
+    contract.respondorStatus = updateRespContract.newStatus;
+    // Get the asset registry that stores the assets. Note that
+    // getAssetRegistry() returns a promise, so we have to await for it.
+    let assetRegistry = await getAssetRegistry('org.hyperledger_composer.scms.Contract');
+    
+    await assetRegistry.update(contract);
+  }
+  
+  /**
+   * Update available number of spare part to project site function.
+   * @param {org.hyperledger_composer.scms.UpdateAvailNoOfSparePartToProjSite} updateAvailNoOfSparePartToProjSite
+   * @transaction
+   */
+  async function updateAvailNoOfSparePartToProjSite(updateAvailNoOfSparePartToProjSite) {  
+    //Update the available number of spare part
+    let sparePartToProjectSite = updateAvailNoOfSparePartToProjSite.sparePartToProjectSite;
+    sparePartToProjectSite.noOfAvailable = updateAvailNoOfSparePartToProjSite.noOfAvailable;
+    // Get the asset registry that stores the assets. Note that
+    // getAssetRegistry() returns a promise, so we have to await for it.
+    let assetRegistry = await getAssetRegistry('org.hyperledger_composer.scms.SparePartToProjectSite');
+    
+    await assetRegistry.update(sparePartToProjectSite);
+  }
+  
